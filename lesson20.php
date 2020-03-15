8f540ac599d1153cfe1d0270b07f91d76190ae4e
user <user@user.user>
2020-03-15 23:08:48 +0700

    lesson20

--- lesson20.php
+++ lesson20.php
@@ -1,14 +1,23 @@
     <?php
-        $i  = 1;
+        $i = 1;
 
-        while ($i <=10){
-            echo $i;
+        while ($i <=10) {
+            echo $i."<br>";
              $i = $i +1;
 
 
     }
     ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
     
+    </body>
+    </html>
 
 
 
